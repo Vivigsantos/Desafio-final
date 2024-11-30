6c33cbd5d00cd874901178d679cb173468821295
Vivigsantos <vgds001@gmail.com>
2024-11-30 12:52:18 +0000

    Adicionei a page livros doados

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,9 +1,20 @@
+import S from "./doados.module.scss"
+import livro from "../../assets/livro.png"
 
 
 export default function Doados(){
     return(
-        <section>
-            <h2>Doados</h2>
+        <section className={S.boxDoados}>
+            <h2>Livros Doados</h2>
+            <section className={S.boxcard}>
+                <article>
+                    <img src={livro} alt="" />
+                    <h3></h3>
+                    <p>O Protagonista</p>
+                    <p>Suzane Andrade</p>
+                    <p>Ficção</p>
+                </article>
+            </section>
         </section>
     )
 }
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -6,14 +6,18 @@ export default function QueroDoar(){
         <section  className={S.principal}>
             <section className={S.container}>
                 <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
-                <form action=""></form>
+                <form action="">
                 <div>
                     <img src={livro} alt="logo do livro da informação" />
-                    <h3>Informações do</h3>                </div>
+                    <h3>Informações do Livro</h3>  
+                    </div>
                     <input type="text" placeholder="Titulo" />
                     <input type="text" placeholder="Categoria"/>
                     <input type="text" placeholder="Autor"/>
                     <input type="text" placeholder="Link da Imagem" />
+                    <input className={S.doar} type="subimit" value="Doar"/>
+                    </form>
+                
             </section>
         </section>
     )
